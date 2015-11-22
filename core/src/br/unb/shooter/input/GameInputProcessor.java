61a47b90cf4954a13cb5dabf3fb50da73ebca5f8
Bruno Schifer <schifers@gmail.com>
2015-11-22 18:39:37 -0200

    xbox

--- core/src/br/unb/shooter/input/GameInputProcessor.java
+++ core/src/br/unb/shooter/input/GameInputProcessor.java
@@ -1,94 +1,150 @@
 package br.unb.shooter.input;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerListener;
+import com.badlogic.gdx.controllers.PovDirection;
+import com.badlogic.gdx.math.Vector3;
 
 import br.unb.shooter.controller.GameController;
 import br.unb.shooter.entity.Player;
 
-public class GameInputProcessor implements InputProcessor {
+public class GameInputProcessor implements InputProcessor, ControllerListener {
 
-    @Override
-    public boolean keyDown(int keycode) {
-        Player player = GameController.getInstance().getPlayer();
+	@Override
+	public boolean keyDown(int keycode) {
+		Player player = GameController.getInstance().getPlayer();
 
-        if (keycode == Keys.W) {
-            player.setMoveUp(true);
-        }
-        if (keycode == Keys.A) {
-            player.setMoveLeft(true);
-        }
-        if (keycode == Keys.S) {
-            player.setMoveDown(true);
-        }
-        if (keycode == Keys.D) {
-            player.setMoveRight(true);
-        }
+		if (keycode == Keys.W) {
+			player.setMoveUp(true);
+		}
+		if (keycode == Keys.A) {
+			player.setMoveLeft(true);
+		}
+		if (keycode == Keys.S) {
+			player.setMoveDown(true);
+		}
+		if (keycode == Keys.D) {
+			player.setMoveRight(true);
+		}
 
-        player.setMovingState();
+		player.setMovingState();
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public boolean keyUp(int keycode) {
-        Player player = GameController.getInstance().getPlayer();
+	@Override
+	public boolean keyUp(int keycode) {
+		Player player = GameController.getInstance().getPlayer();
 
-        if (keycode == Keys.W) {
-            player.setMoveUp(false);
-        }
-        if (keycode == Keys.A) {
-            player.setMoveLeft(false);
-        }
-        if (keycode == Keys.S) {
-            player.setMoveDown(false);
-        }
-        if (keycode == Keys.D) {
-            player.setMoveRight(false);
-        }
+		if (keycode == Keys.W) {
+			player.setMoveUp(false);
+		}
+		if (keycode == Keys.A) {
+			player.setMoveLeft(false);
+		}
+		if (keycode == Keys.S) {
+			player.setMoveDown(false);
+		}
+		if (keycode == Keys.D) {
+			player.setMoveRight(false);
+		}
 
-        player.setMovingState();
+		player.setMovingState();
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public boolean keyTyped(char character) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        GameController.getInstance().getPlayer().setIsShooting(true);
-        GameController.getInstance().getPlayer().setTargetX(Float.valueOf(screenX));
-        GameController.getInstance().getPlayer().setTargetY(Float.valueOf(screenY));
-        return false;
-    }
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		GameController.getInstance().getPlayer().setIsShooting(true);
+		GameController.getInstance().getPlayer().setTargetX(Float.valueOf(screenX));
+		GameController.getInstance().getPlayer().setTargetY(Float.valueOf(screenY));
+		return false;
+	}
 
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        GameController.getInstance().setMouseX(Float.valueOf(screenX));
-        GameController.getInstance().setMouseY(Float.valueOf(screenY));
-        return false;
-    }
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		GameController.getInstance().setMouseX(Float.valueOf(screenX));
+		GameController.getInstance().setMouseY(Float.valueOf(screenY));
+		return false;
+	}
 
-    @Override
-    public boolean scrolled(int amount) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void connected(Controller controller) {
+		Gdx.app.log("XBOX", "connected");
+	}
+
+	@Override
+	public void disconnected(Controller controller) {
+		Gdx.app.log("XBOX", "connected");
+	}
+
+	@Override
+	public boolean buttonDown(Controller controller, int buttonCode) {
+		Gdx.app.log("XBOX", Integer.toString(buttonCode));
+		return false;
+	}
+
+	@Override
+	public boolean buttonUp(Controller controller, int buttonCode) {
+		Gdx.app.log("XBOX", Integer.toString(buttonCode));
+		return false;
+	}
+
+	@Override
+	public boolean axisMoved(Controller controller, int axisCode, float value) {
+		Gdx.app.log("XBOX", "axis: " + Integer.toString(axisCode) + " value: " + value);
+		return false;
+	}
+
+	@Override
+	public boolean povMoved(Controller controller, int povCode, PovDirection value) {
+		Gdx.app.log("XBOX", "pov: " + Integer.toString(povCode) + " value: " + value);
+		return false;
+	}
+
+	@Override
+	public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
+		Gdx.app.log("XBOX", "slider: " + Integer.toString(sliderCode) + " value: " + value);
+		return false;
+	}
+
+	@Override
+	public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
+		Gdx.app.log("XBOX", "slider: " + Integer.toString(sliderCode) + " value: " + value);
+		return false;
+	}
+
+	@Override
+	public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
+		return false;
+	}
 
 }
--- core/src/br/unb/shooter/screen/GameScreen.java
+++ core/src/br/unb/shooter/screen/GameScreen.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
@@ -22,180 +23,184 @@ import br.unb.shooter.input.GameInputProcessor;
 
 public class GameScreen extends Screen {
 
-    private SpriteBatch batch;
-    private OrthographicCamera camera;
-    private Viewport viewport;
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private Viewport viewport;
 
-    private DebugGdx debugGdx;
+	private DebugGdx debugGdx;
 
-    private PlayerCollision playerCollision;
+	private PlayerCollision playerCollision;
 
-    /**
-     * Constructor.
-     */
-    public GameScreen() {
-        super();
-    }
+	/**
+	 * Constructor.
+	 */
+	public GameScreen() {
+		super();
+	}
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        viewport = new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		viewport = new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        GdxController.getInstance().getPlayerGdx().initGraphics();
+		GdxController.getInstance().getPlayerGdx().initGraphics();
 
-        GdxController.getInstance().getEnemyGdx().initGraphics();
+		GdxController.getInstance().getEnemyGdx().initGraphics();
 
-        GdxController.getInstance().getWeaponGdx().initGraphics();
+		GdxController.getInstance().getWeaponGdx().initGraphics();
 
-        GdxController.getInstance().getMarkGdx().initGraphics();
+		GdxController.getInstance().getMarkGdx().initGraphics();
 
-        GdxController.getInstance().getShotGdx().initGraphics();
+		GdxController.getInstance().getShotGdx().initGraphics();
 
-        GdxController.getInstance().getMapGdx().initGraphics();
+		GdxController.getInstance().getMapGdx().initGraphics();
 
-        Gdx.input.setInputProcessor(new GameInputProcessor());
+		GameInputProcessor input = new GameInputProcessor();
 
-        Gdx.input.setCursorImage(GdxController.getInstance().getMarkGdx().getPixmap(), 16, 13);
+		Gdx.input.setInputProcessor(input);
 
-        playerCollision = GameController.getInstance().getMovement();
-        playerCollision.setPlayer(GameController.getInstance().getPlayer());
+		Controllers.addListener(input);
 
-        // debugGdx = new DebugGdx();
-    }
+		Gdx.input.setCursorImage(GdxController.getInstance().getMarkGdx().getPixmap(), 16, 13);
 
-    /**
-     * Updates the screen.
-     */
-    @Override
-    public void update() {
-        Vector3 mousePosition = new Vector3(GameController.getInstance().getMouseX(),
-                GameController.getInstance().getMouseY(), 0);
+		playerCollision = GameController.getInstance().getMovement();
+		playerCollision.setPlayer(GameController.getInstance().getPlayer());
 
-        camera.unproject(mousePosition);
+		// debugGdx = new DebugGdx();
+	}
 
-        GameController.getInstance().getPlayer().setTargetX(mousePosition.x);
-        GameController.getInstance().getPlayer().setTargetY(mousePosition.y);
+	/**
+	 * Updates the screen.
+	 */
+	@Override
+	public void update() {
+		Vector3 mousePosition = new Vector3(GameController.getInstance().getMouseX(),
+				GameController.getInstance().getMouseY(), 0);
 
-        playerCollision.saveOldPosition();
+		camera.unproject(mousePosition);
 
-        // Updates players.
-        for (Player player : GameController.getInstance().getPlayersMap().values()) {
-            player.update();
-            GdxController.getInstance().getPlayerGdx().update(player, Gdx.graphics.getDeltaTime());
-            if (player.getIsShooting()) {
-                GameController.getInstance().createShot(player);
-            }
-        }
+		GameController.getInstance().getPlayer().setTargetX(mousePosition.x);
+		GameController.getInstance().getPlayer().setTargetY(mousePosition.y);
 
-        playerCollision.update();
+		playerCollision.saveOldPosition();
 
-        // Updates the weapon
-        for (FireWeapon weapon : GameController.getInstance().getWeaponsMap().values()) {
-            weapon.update();
-        }
+		// Updates players.
+		for (Player player : GameController.getInstance().getPlayersMap().values()) {
+			player.update();
+			GdxController.getInstance().getPlayerGdx().update(player, Gdx.graphics.getDeltaTime());
+			if (player.getIsShooting()) {
+				GameController.getInstance().createShot(player);
+			}
+		}
 
-        // Updates the enemies
-        for (Enemy enemy : GameController.getInstance().getEnemiesMap().values()) {
-            // enemy.update();
-            GdxController.getInstance().getEnemyGdx().update(enemy, Gdx.graphics.getDeltaTime());
-        }
+		playerCollision.update();
 
-        // Updates shots.
-        List<Integer> ids = new ArrayList<Integer>();
-        for (Shot shot : GameController.getInstance().getShotsMap().values()) {
-            shot.update();
-            if (shot.getFinish()) {
-                ids.add(shot.getId());
-                GameController.getInstance().getRemovedShots().add(shot.getId());
-            }
-        }
-        for (Integer id : ids) {
-            GameController.getInstance().getShotsMap().remove(id);
-        }
+		// Updates the weapon
+		for (FireWeapon weapon : GameController.getInstance().getWeaponsMap().values()) {
+			weapon.update();
+		}
 
-        // Updates map.
-        GdxController.getInstance().getMapGdx().update();
+		// Updates the enemies
+		for (Enemy enemy : GameController.getInstance().getEnemiesMap().values()) {
+			// enemy.update();
+			GdxController.getInstance().getEnemyGdx().update(enemy, Gdx.graphics.getDeltaTime());
+		}
 
-        float oldCameraX = camera.position.x;
-        float oldCameraY = camera.position.y;
+		// Updates shots.
+		List<Integer> ids = new ArrayList<Integer>();
+		for (Shot shot : GameController.getInstance().getShotsMap().values()) {
+			shot.update();
+			if (shot.getFinish()) {
+				ids.add(shot.getId());
+				GameController.getInstance().getRemovedShots().add(shot.getId());
+			}
+		}
+		for (Integer id : ids) {
+			GameController.getInstance().getShotsMap().remove(id);
+		}
 
-        camera.position.x = GameController.getInstance().getPlayer().getX()
-                + GameController.getInstance().getPlayer().getOffsetX();
-        camera.position.y = GameController.getInstance().getPlayer().getY()
-                + GameController.getInstance().getPlayer().getOffsetY();
+		// Updates map.
+		GdxController.getInstance().getMapGdx().update();
 
-        if (camera.position.x < (camera.viewportWidth / 2)) {
-            camera.position.x = oldCameraX;
-        }
-        if (camera.position.y < (camera.viewportHeight / 2)) {
-            camera.position.y = oldCameraY;
-        }
+		float oldCameraX = camera.position.x;
+		float oldCameraY = camera.position.y;
 
-        float mapWidth = GdxController.getInstance().getMapGdx().getForeground().getTileWidth()
-                * GdxController.getInstance().getMapGdx().getForeground().getWidth();
-        float mapHeight = GdxController.getInstance().getMapGdx().getForeground().getTileHeight()
-                * GdxController.getInstance().getMapGdx().getForeground().getHeight();
+		camera.position.x = GameController.getInstance().getPlayer().getX()
+				+ GameController.getInstance().getPlayer().getOffsetX();
+		camera.position.y = GameController.getInstance().getPlayer().getY()
+				+ GameController.getInstance().getPlayer().getOffsetY();
 
-        if (camera.position.x < (camera.viewportWidth / 2)) {
-            camera.position.x = oldCameraX;
-        }
-        if (camera.position.y < (camera.viewportHeight / 2)) {
-            camera.position.y = oldCameraY;
-        }
-        if (camera.position.x > (mapWidth - (camera.viewportWidth / 2))) {
-            camera.position.x = oldCameraX;
-        }
-        if (camera.position.y > (mapHeight - (camera.viewportHeight / 2))) {
-            camera.position.y = oldCameraY;
-        }
+		if (camera.position.x < (camera.viewportWidth / 2)) {
+			camera.position.x = oldCameraX;
+		}
+		if (camera.position.y < (camera.viewportHeight / 2)) {
+			camera.position.y = oldCameraY;
+		}
 
-        camera.update();
+		float mapWidth = GdxController.getInstance().getMapGdx().getForeground().getTileWidth()
+				* GdxController.getInstance().getMapGdx().getForeground().getWidth();
+		float mapHeight = GdxController.getInstance().getMapGdx().getForeground().getTileHeight()
+				* GdxController.getInstance().getMapGdx().getForeground().getHeight();
 
-        // debugGdx.update(player, GameController.getInstance().getMouseX(),
-        // GameController.getInstance().getMouseY());
+		if (camera.position.x < (camera.viewportWidth / 2)) {
+			camera.position.x = oldCameraX;
+		}
+		if (camera.position.y < (camera.viewportHeight / 2)) {
+			camera.position.y = oldCameraY;
+		}
+		if (camera.position.x > (mapWidth - (camera.viewportWidth / 2))) {
+			camera.position.x = oldCameraX;
+		}
+		if (camera.position.y > (mapHeight - (camera.viewportHeight / 2))) {
+			camera.position.y = oldCameraY;
+		}
 
-    }
+		camera.update();
 
-    /**
-     * Renders game screen.
-     */
-    @Override
-    public void draw() {
-        super.draw();
+		// debugGdx.update(player, GameController.getInstance().getMouseX(),
+		// GameController.getInstance().getMouseY());
 
-        GdxController.getInstance().getMapGdx().draw(camera);
+	}
 
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        for (Enemy enemy : GameController.getInstance().getEnemiesMap().values()) {
-            GdxController.getInstance().getEnemyGdx().draw(batch, enemy);
-        }
-        for (Player player : GameController.getInstance().getPlayersMap().values()) {
-            GdxController.getInstance().getPlayerGdx().draw(batch, player);
-        }
-        for (Shot shot : GameController.getInstance().getShotsMap().values()) {
-            GdxController.getInstance().getShotGdx().draw(batch, shot);
-        }
-        for (FireWeapon weapon : GameController.getInstance().getWeaponsMap().values()) {
-            GdxController.getInstance().getWeaponGdx().draw(batch, weapon);
-        }
-        batch.end();
+	/**
+	 * Renders game screen.
+	 */
+	@Override
+	public void draw() {
+		super.draw();
 
-        // debugGdx.draw(camera);
+		GdxController.getInstance().getMapGdx().draw(camera);
 
-    }
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		for (Enemy enemy : GameController.getInstance().getEnemiesMap().values()) {
+			GdxController.getInstance().getEnemyGdx().draw(batch, enemy);
+		}
+		for (Player player : GameController.getInstance().getPlayersMap().values()) {
+			GdxController.getInstance().getPlayerGdx().draw(batch, player);
+		}
+		for (Shot shot : GameController.getInstance().getShotsMap().values()) {
+			GdxController.getInstance().getShotGdx().draw(batch, shot);
+		}
+		for (FireWeapon weapon : GameController.getInstance().getWeaponsMap().values()) {
+			GdxController.getInstance().getWeaponGdx().draw(batch, weapon);
+		}
+		batch.end();
 
-    public void dispose() {
-        batch.dispose();
-        debugGdx.dispose();
-    }
+		// debugGdx.draw(camera);
 
-    @Override
-    public void resize(int width, int height) {
-        viewport.update(width, height);
-    }
+	}
+
+	public void dispose() {
+		batch.dispose();
+		debugGdx.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height);
+	}
 
 }
