a6683affce52709a892f2270f8d034e2e44a1fe3
Bruno Schifer <schifers@gmail.com>
2015-11-23 23:18:44 -0200

    fix project

--- core/src/br/unb/shooter/input/GameInputProcessor.java
+++ core/src/br/unb/shooter/input/GameInputProcessor.java
@@ -10,164 +10,225 @@ import com.badlogic.gdx.math.Vector3;
 
 import br.unb.shooter.controller.GameController;
 import br.unb.shooter.entity.Player;
+import br.unb.shooter.entity.TargetMark;
+import br.unb.shooter.gamepad.XBox360Pad;
 
 public class GameInputProcessor implements InputProcessor, ControllerListener {
 
-	@Override
-	public boolean keyDown(int keycode) {
-		Player player = GameController.getInstance().getPlayer();
+    @Override
+    public boolean keyDown(int keycode) {
+        Player player = GameController.getInstance().getPlayer();
 
-		if (keycode == Keys.W) {
-			player.setMoveUp(true);
-		}
-		if (keycode == Keys.A) {
-			player.setMoveLeft(true);
-		}
-		if (keycode == Keys.S) {
-			player.setMoveDown(true);
-		}
-		if (keycode == Keys.D) {
-			player.setMoveRight(true);
-		}
+        if (keycode == Keys.W) {
+            player.setMoveUp(true);
+        }
+        if (keycode == Keys.A) {
+            player.setMoveLeft(true);
+        }
+        if (keycode == Keys.S) {
+            player.setMoveDown(true);
+        }
+        if (keycode == Keys.D) {
+            player.setMoveRight(true);
+        }
 
-		player.setMovingState();
+        player.setMovingState();
 
-		return false;
-	}
+        return false;
+    }
 
-	@Override
-	public boolean keyUp(int keycode) {
-		Player player = GameController.getInstance().getPlayer();
+    @Override
+    public boolean keyUp(int keycode) {
+        Player player = GameController.getInstance().getPlayer();
 
-		if (keycode == Keys.W) {
-			player.setMoveUp(false);
-		}
-		if (keycode == Keys.A) {
-			player.setMoveLeft(false);
-		}
-		if (keycode == Keys.S) {
-			player.setMoveDown(false);
-		}
-		if (keycode == Keys.D) {
-			player.setMoveRight(false);
-		}
+        if (keycode == Keys.W) {
+            player.setMoveUp(false);
+        }
+        if (keycode == Keys.A) {
+            player.setMoveLeft(false);
+        }
+        if (keycode == Keys.S) {
+            player.setMoveDown(false);
+        }
+        if (keycode == Keys.D) {
+            player.setMoveRight(false);
+        }
 
-		player.setMovingState();
+        player.setMovingState();
 
-		return false;
-	}
+        return false;
+    }
 
-	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean keyTyped(char character) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		GameController.getInstance().getPlayer().setIsShooting(true);
-		GameController.getInstance().getPlayer().setTargetX(Float.valueOf(screenX));
-		GameController.getInstance().getPlayer().setTargetY(Float.valueOf(screenY));
-		return false;
-	}
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        GameController.getInstance().getPlayer().setIsShooting(true);
+        return false;
+    }
 
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		GameController.getInstance().setMouseX(Float.valueOf(screenX));
-		GameController.getInstance().setMouseY(Float.valueOf(screenY));
-		return false;
-	}
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        GameController.getInstance().setMouseX(Float.valueOf(screenX));
+        GameController.getInstance().setMouseY(Float.valueOf(screenY));
+        return false;
+    }
 
-	@Override
-	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean scrolled(int amount) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public void connected(Controller controller) {
-		Gdx.app.log("XBOX", "connected");
-	}
+    @Override
+    public void connected(Controller controller) {
+        Gdx.app.log("XBOX", "connected");
+    }
 
-	@Override
-	public void disconnected(Controller controller) {
-		Gdx.app.log("XBOX", "connected");
-	}
+    @Override
+    public void disconnected(Controller controller) {
+        Gdx.app.log("XBOX", "disconnected");
+    }
 
-	@Override
-	public boolean buttonDown(Controller controller, int buttonCode) {
-		Gdx.app.log("XBOX", Integer.toString(buttonCode));
-		return false;
-	}
+    @Override
+    public boolean buttonDown(Controller controller, int buttonCode) {
+        Player player = GameController.getInstance().getPlayer();
 
-	@Override
-	public boolean buttonUp(Controller controller, int buttonCode) {
-		Gdx.app.log("XBOX", Integer.toString(buttonCode));
-		return false;
-	}
+        // up
+        if (buttonCode == 0) {
+            player.setMoveUp(true);
+        }
+        // down
+        if (buttonCode == 1) {
+            player.setMoveDown(true);
+        }
+        // left
+        if (buttonCode == 2) {
+            player.setMoveLeft(true);
+        }
+        // right
+        if (buttonCode == 3) {
+            player.setMoveRight(true);
+        }
+        // shoot
+        if (buttonCode == 9) {
+            GameController.getInstance().getPlayer().setIsShooting(true);
+        }
 
-	@Override
-	public boolean axisMoved(Controller controller, int axisCode, float value) {
-		Player player = GameController.getInstance().getPlayer();
+        player.setMovingState();
 
-		// left stick - axis 1 = left/right
-		if (axisCode == 1 && value > 0.5) {
-			player.setMoveRight(true);
-		} else if (axisCode == 1 && value < 0.5) {
-			player.setMoveRight(false);
-		}
-		if (axisCode == 1 && value < -0.5) {
-			player.setMoveLeft(true);
-		} else if (axisCode == 1 && value > -0.5) {
-			player.setMoveLeft(false);
-		}
-		// left stick - axis 0 = down/up - axis 0 = up/down
-		if (axisCode == 0 && value > 0.5) {
-			player.setMoveDown(true);
-		} else if (axisCode == 0 && value < 0.5) {
-			player.setMoveDown(false);
-		}
-		if (axisCode == 0 && value < -0.5) {
-			player.setMoveUp(true);
-		} else if (axisCode == 0 && value > -0.5) {
-			player.setMoveUp(false);
-		}
-		return false;
-	}
+        return false;
+    }
 
-	@Override
-	public boolean povMoved(Controller controller, int povCode, PovDirection value) {
-		Gdx.app.log("XBOX", "pov: " + Integer.toString(povCode) + " value: " + value);
-		return false;
-	}
+    @Override
+    public boolean buttonUp(Controller controller, int buttonCode) {
+        Player player = GameController.getInstance().getPlayer();
 
-	@Override
-	public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
-		Gdx.app.log("XBOX", "slider: " + Integer.toString(sliderCode) + " value: " + value);
-		return false;
-	}
+        // up
+        if (buttonCode == 0) {
+            player.setMoveUp(false);
+        }
+        // down
+        if (buttonCode == 1) {
+            player.setMoveDown(false);
+        }
+        // left
+        if (buttonCode == 2) {
+            player.setMoveLeft(false);
+        }
+        // right
+        if (buttonCode == 3) {
+            player.setMoveRight(false);
+        }
 
-	@Override
-	public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
-		Gdx.app.log("XBOX", "slider: " + Integer.toString(sliderCode) + " value: " + value);
-		return false;
-	}
+        player.setMovingState();
 
-	@Override
-	public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
-		return false;
-	}
+        return false;
+    }
+
+    @Override
+    public boolean axisMoved(Controller controller, int axisCode, float value) {
+        Player player = GameController.getInstance().getPlayer();
+        TargetMark targetMark = GameController.getInstance().getTargetMark();
+
+        if (axisCode == XBox360Pad.AXIS_LEFT_X && value > 0.5) {
+            player.setMoveRight(true);
+        } else if (axisCode == XBox360Pad.AXIS_LEFT_X && value < 0.5) {
+            player.setMoveRight(false);
+        }
+        if (axisCode == XBox360Pad.AXIS_LEFT_X && value < -0.5) {
+            player.setMoveLeft(true);
+        } else if (axisCode == XBox360Pad.AXIS_LEFT_X && value > -0.5) {
+            player.setMoveLeft(false);
+        }
+        if (axisCode == XBox360Pad.AXIS_LEFT_Y && value > 0.5) {
+            player.setMoveDown(true);
+        } else if (axisCode == XBox360Pad.AXIS_LEFT_Y && value < 0.5) {
+            player.setMoveDown(false);
+        }
+        if (axisCode == XBox360Pad.AXIS_LEFT_Y && value < -0.5) {
+            player.setMoveUp(true);
+        } else if (axisCode == XBox360Pad.AXIS_LEFT_Y && value > -0.5) {
+            player.setMoveUp(false);
+        }
+
+        // Target mark
+        if (axisCode == XBox360Pad.AXIS_RIGHT_X && value > 0) {
+            targetMark.setMoveRight(true);
+            targetMark.setIntensityX(value);
+        }
+        if (axisCode == XBox360Pad.AXIS_RIGHT_X && value < 0) {
+            targetMark.setMoveLeft(true);
+            targetMark.setIntensityX(value);
+        }
+        if (axisCode == XBox360Pad.AXIS_RIGHT_Y && value > 0) {
+            targetMark.setMoveDown(true);
+            targetMark.setIntensityY(value);
+        }
+        if (axisCode == XBox360Pad.AXIS_RIGHT_Y && value < 0) {
+            targetMark.setMoveUp(true);
+            targetMark.setIntensityY(value);
+        }
+
+        player.setMovingState();
+
+        return false;
+    }
+
+    @Override
+    public boolean povMoved(Controller controller, int povCode, PovDirection value) {
+        return false;
+    }
+
+    @Override
+    public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
+        return false;
+    }
+
+    @Override
+    public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
+        return false;
+    }
+
+    @Override
+    public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
+        return false;
+    }
 
 }
--- core/src/br/unb/shooter/util/Constants.java
+++ core/src/br/unb/shooter/util/Constants.java
@@ -1,16 +1,16 @@
 package br.unb.shooter.util;
 
 public class Constants {
-    public static final String SPACE = " ";
-    public static final String ZERO = "0";
-    public static final String ONE = "1";
-    public static final int CAMERA_WIDTH = 600;
-    public static final int CAMERA_HEIGHT = 600;
-    public static final String NEW_LINE = "\n";
+	public static final String SPACE = " ";
+	public static final String ZERO = "0";
+	public static final String ONE = "1";
+	public static final int CAMERA_WIDTH = 600;
+	public static final int CAMERA_HEIGHT = 600;
+	public static final String NEW_LINE = "\n";
 
-    public static final Boolean CONTROLLER = true;
+	public static final Boolean CONTROLLER = false;
 
-    public static String convertBoolean(Boolean bool) {
-        return bool ? "1" : "0";
-    }
+	public static String convertBoolean(Boolean bool) {
+		return bool ? "1" : "0";
+	}
 }
